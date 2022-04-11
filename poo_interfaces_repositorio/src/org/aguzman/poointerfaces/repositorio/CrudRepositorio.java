bac6161528ce94e81ffea39cde84008b4480fe2d
Rych Daniels <rychraya@gmail.com>
2022-04-11 17:04:51 -0500

    Sección 21: Programación orientada a objetos: Ejemplo Interface Repositorio, terminada

---
+++ poo_interfaces_repositorio/src/org/aguzman/poointerfaces/repositorio/CrudRepositorio.java
@@ -0,0 +1,13 @@
+package org.aguzman.poointerfaces.repositorio;
+
+import org.aguzman.poointerfaces.modelo.Cliente;
+
+import java.util.List;
+
+public interface CrudRepositorio {
+    List<Cliente> listar();
+    Cliente porId(Integer id);
+    void crear(Cliente cliente);
+    void editar(Cliente cliente);
+    void eliminar(Integer id);
+}
