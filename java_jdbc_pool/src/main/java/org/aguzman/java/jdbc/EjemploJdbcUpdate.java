88395fb6c684540dd9181b7e08102678508e8e94
Rych Daniels <rychraya@gmail.com>
2022-05-03 20:50:40 -0500

    Sección 35: Bases de datos con JDBC: Pool de conexiones, terminada

---
+++ java_jdbc_pool/src/main/java/org/aguzman/java/jdbc/EjemploJdbcUpdate.java
@@ -0,0 +1,36 @@
+package org.aguzman.java.jdbc;
+
+import org.aguzman.java.jdbc.modelo.Categoria;
+import org.aguzman.java.jdbc.modelo.Producto;
+import org.aguzman.java.jdbc.repositorio.ProductoRepositorioImpl;
+import org.aguzman.java.jdbc.repositorio.Repositorio;
+import org.aguzman.java.jdbc.util.ConexionBaseDatos;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class EjemploJdbcUpdate {
+    public static void main(String[] args) {
+
+            Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
+            System.out.println("============= listar =============");
+            repositorio.listar().forEach(System.out::println);
+
+            System.out.println("============= obtener por id =============");
+            System.out.println(repositorio.porId(1L));
+
+            System.out.println("============= editar producto =============");
+            Producto producto = new Producto();
+            producto.setId(5L);
+            producto.setNombre("Teclado Cosair k95 mecánico");
+            producto.setPrecio(700);
+            Categoria categoria = new Categoria();
+            categoria.setId(2L);
+            producto.setCategoria(categoria);
+            repositorio.guardar(producto);
+            System.out.println("Producto editado con éxito");
+            repositorio.listar().forEach(System.out::println);
+
+    }
+}
