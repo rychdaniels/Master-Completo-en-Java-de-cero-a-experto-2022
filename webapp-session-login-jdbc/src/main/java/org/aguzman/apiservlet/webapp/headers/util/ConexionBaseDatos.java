156fd8ecbd0af48525a8b65e1ed5bb13062943fc
Rych Daniels <rychraya@gmail.com>
2022-07-19 19:52:32 -0500

    Se modifican credenciales para funcionar en locahost

--- webapp-session-login-jdbc/src/main/java/org/aguzman/apiservlet/webapp/headers/util/ConexionBaseDatos.java
+++ webapp-session-login-jdbc/src/main/java/org/aguzman/apiservlet/webapp/headers/util/ConexionBaseDatos.java
@@ -8,7 +8,7 @@ public class ConexionBaseDatos {
 
     private static String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=America/Santiago";
     private static String username = "root";
-    private static String password = "sasa";
+    private static String password = "root";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, username, password);
